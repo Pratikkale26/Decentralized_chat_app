eec025a9915115ae87c2abbdb811480ba131f772
Pratik Kale <147796613+PKBOSS26@users.noreply.github.com>
2025-03-27 14:59:46 +0530

    minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 'use client';
 
 import { useContext } from 'react';
-import { ChatAppContext } from '@/Components/ChatAppContext';
+import { ChatAppContext } from '@/Context/ChatAppContext';
 
 export default function Home() {
-  const { title } = useContext(ChatAppContext);
+  const chatContext = useContext(ChatAppContext);
+
+  if (!chatContext) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-        <h1 className="text-4xl font-bold">{title}</h1>
+        <h1 className="text-4xl font-bold">
+          {chatContext.userName ? chatContext.userName : 'guest'}
+        </h1>
     </div>
   );
 }
